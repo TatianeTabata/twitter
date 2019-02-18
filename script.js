b6a241c90a886358f3e5233896cd47f3f98b5f23
TatianeTabata <tatianetabata@hotmail.com>
2019-02-18 19:57:58 -0300

    versao 0.0.2

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
 
 function showMessage(event){
+    if(document.getElementById("textbox").value < 1){
+    alert("Sua mensagem não contem caracteres, escreva uma mensagem.")
+    }
     event.preventDefault();
     let message = document.getElementById("textbox").value;
     document.getElementById("display-message").innerHTML = message;
 }
 
+function countChar(valor) {
+  let quant = 140;
+  let total = valor.length;
+  if(total <= quant) {
+      let resto = quant - total;
+      document.getElementById('cont').innerHTML = resto;
+  } else {
+      document.getElementById('textbox').value = valor.substr(0, quant);
+  }
+}
 
+/*  function countChar(message, value){
+    let message = document.getElementById("textbox").value;
+    let counter = message.lenght;
+    let charMax = 140;
 
+    for(i=0; i<charMax; i++){
+      if(counter < charMax){
 
-/*function send2(){
-    let deslocamento = document.getElementById("deslocId2").value;
-    deslocamento = parseInt(deslocamento);
-    let message = document.getElementById("messageIdDecifra").value;
-    let result = cipher.decode(message, deslocamento);
-    document.getElementById("msgDecifrada").innerHTML = "Mensagem cifrada é: " + "<br />" + result;
-  }*/
+    }
+}
+}*/
+
