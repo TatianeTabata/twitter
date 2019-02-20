4ec788526b2123349a8edb87f959966acc2cc3e5
TatianeTabata <tatianetabata@hotmail.com>
2019-02-19 22:15:07 -0300

    versao 0.0.2 contador negativo

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 
-
 function showMessage(event){
-    if(document.getElementById("textbox").value < 1){
-    alert("Sua mensagem não contem caracteres, escreva uma mensagem.")
-    }
-    event.preventDefault();
-    let message = document.getElementById("textbox").value;
-    document.getElementById("display-message").innerHTML = message;
-}
-
-function countChar(valor) {
-  let quant = 140;
-  let total = valor.length;
-  if(total <= quant) {
-      let resto = quant - total;
-      document.getElementById('cont').innerHTML = resto;
-  } else {
-      document.getElementById('textbox').value = valor.substr(0, quant);
+  if(document.getElementById("textbox").value < 1){
+  alert("Sua mensagem não contem caracteres, escreva uma mensagem.")
   }
+  event.preventDefault();
+  let message = document.getElementById("textbox").value;
+  // document.getElementById("display-message").innerHTML = message;
 }
 
-/*  function countChar(message, value){
-    let message = document.getElementById("textbox").value;
-    let counter = message.lenght;
-    let charMax = 140;
+document.getElementById("tweet").disabled = true;
 
-    for(i=0; i<charMax; i++){
-      if(counter < charMax){
-
-    }
+function countChar(valor) {  
+  let quant = 140;
+  let total = valor.length; 
+  let resto = 0;     
+  
+  if(total <= quant && total > 0) {
+    document.getElementById("tweet").disabled = false;
+    resto = quant - total;
+    document.getElementById('cont').innerHTML = resto;
+  } if(total > quant) {
+    resto = - (total - quant); 
+    document.getElementById('cont').innerHTML = resto;
+    document.getElementById("tweet").disabled = true;
+  } 
 }
-}*/
+
+tweet.addEventListener("click", showMessage);
+tweet.addEventListener("click", myFunction);
+
+let entrada = document.getElementById("textbox");
+let saida = document.getElementById("display-message");
+
+function myFunction(){  
+  let newP = document.createElement('p');  
+  newP.innerHTML = entrada.value;
+  saida.appendChild(newP);    
+  entrada.value = '';  
+}
 
--- teste.js
+++ teste.js
@@ -1,10 +1,40 @@
-function limite_textarea(valor) {
-  quant = 50;
-  total = valor.length;
-  if(total <= quant) {
-      resto = quant - total;
-      document.getElementById('cont').innerHTML = resto;
-  } else {
-      document.getElementById('texto').value = valor.substr(0,quant);
+
+function showMessage(event){
+  if(document.getElementById("textbox").value < 1){
+  alert("Sua mensagem não contem caracteres, escreva uma mensagem.")
   }
+  event.preventDefault();
+  let message = document.getElementById("textbox").value;
+  // document.getElementById("display-message").innerHTML = message;
+  
 }
+
+document.getElementById("tweet").disabled = true;
+
+function countChar(valor) {  
+  let quant = 140;
+  let total = valor.length;      
+  if(total < quant && total > 0) {
+    document.getElementById("tweet").disabled = false;
+    let resto = quant - total;
+    document.getElementById('cont').innerHTML = resto;
+  } else {
+    document.getElementById('textbox').value = valor.substr(0, quant);
+    document.getElementById("tweet").disabled = true;
+  }  
+}
+
+tweet.addEventListener("click", showMessage);
+tweet.addEventListener("click", myFunction);
+
+let entrada = document.getElementById("textbox");
+let saida = document.getElementById("display-message");
+
+function myFunction(){  
+  let newP = document.createElement('p');  
+  newP.innerHTML = entrada.value;
+  saida.insertBefore(newP, saida)
+  saida.appendChild(newP);    
+  entrada.value = '';  
+}
+
